51d261dce88c21b9915aa5cd69fe4480c99b10ba
AVC <151655878+AlexandreVC@users.noreply.github.com>
2023-12-12 17:22:15 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -28,7 +28,7 @@ char *targetDir(char *env, char *path)
 int main(int argc, char **argv)
 {
     // La fonction targetdir décalée ici
-    char *div;	
+    char *dir;	
     char *dirEnv;
     char *env = "PWD";
     char *path = "/18n/";
