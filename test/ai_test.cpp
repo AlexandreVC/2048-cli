32050d1da8d1974009411040d9617e7dec58a4fb
AVC <151655878+AlexandreVC@users.noreply.github.com>
2023-12-13 21:53:48 +0100

    Update ai_test.cpp

--- test/ai_test.cpp
+++ test/ai_test.cpp
@@ -2,13 +2,11 @@
 #include "ai.h"
 #include "engine.h"
 
-// No need to redefine 'struct gamestate' and 'struct options' as they are already defined in 'engine.h'
+
 
 // Test to check if ai_move returns a valid move
 TEST(AIMoveTest, ReturnsValidMove) {
     struct gamestate g;
-    // Assuming 'g.opts' is already properly initialized in 'engine.h'
-    // If 'opts' needs to be set, it should be set according to the definition in 'engine.h'
 
     char move = ai_move(&g);
     EXPECT_TRUE(move == 'w' || move == 'a' || move == 's' || move == 'd');
@@ -17,7 +15,6 @@ TEST(AIMoveTest, ReturnsValidMove) {
 // Test to check behavior when interactive option is enabled
 TEST(AIMoveTest, InteractiveOption) {
     struct gamestate g;
-    // Assuming 'g.opts' is already properly initialized in 'engine.h'
 
     char move = ai_move(&g);
     EXPECT_TRUE(move == 'w' || move == 'a' || move == 's' || move == 'd');
