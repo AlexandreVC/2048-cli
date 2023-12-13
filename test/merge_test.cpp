6f804f30d7661d8e1aa5798ec1af63a89dc8c8d2
AVC <151655878+AlexandreVC@users.noreply.github.com>
2023-12-13 21:56:27 +0100

    Update merge_test.cpp

--- test/merge_test.cpp
+++ test/merge_test.cpp
@@ -1,13 +1,11 @@
 #include <gtest/gtest.h>
 #include "merge.h"
 
-// Assuming merge_std.c or merge_fib.c provides the implementation
 // Test for merge_value function
 TEST(MergeTest, MergeValue) {
     // Check if the value of index 0 is 0 (empty)
     EXPECT_EQ(merge_value(0), 0);
 
-    // Additional checks can be added based on the actual implementation
 }
 
 // Test for merge_goal function
@@ -15,7 +13,6 @@ TEST(MergeTest, MergeGoal) {
     // Check if the goal index value is a specific expected value
     EXPECT_EQ(merge_goal(), expected_goal_value);
 
-    // Additional checks can be added based on the actual implementation
 }
 
 // Test for merge_possible function
@@ -23,7 +20,6 @@ TEST(MergeTest, MergePossible) {
     // Check if a merge is possible between two specific indices
     EXPECT_TRUE(merge_possible(index1, index2));
 
-    // Additional checks can be added based on the actual implementation
 }
 
 // Test for merge_result function
@@ -31,7 +27,6 @@ TEST(MergeTest, MergeResult) {
     // Check the result of merging two specific values
     EXPECT_EQ(merge_result(value1, value2), expected_result);
 
-    // Additional checks can be added based on the actual implementation
 }
 
 int main(int argc, char **argv) {
