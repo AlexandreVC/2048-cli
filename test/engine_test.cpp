5a3dc07750a5eb19c7c6ac8308730a4d90c04767
AVC <151655878+AlexandreVC@users.noreply.github.com>
2023-12-13 21:53:25 +0100

    Update engine_test.cpp

--- test/engine_test.cpp
+++ test/engine_test.cpp
@@ -4,9 +4,7 @@
 // Test to check initial state of the game
 TEST(GameEngineTest, InitialState) {
     struct gamestate g;
-    // Assuming gamestate has a properly defined opts structure
-    // Initialize opts with appropriate values if needed
-    // For example, if opts has grid_width and grid_height, initialize them
+
 
     g->opts->grid_width = 4;
     g->opts->grid_height = 4;
@@ -26,7 +24,6 @@ TEST(GameEngineTest, InitialState) {
     }
 }
 
-// Additional tests can be added to check other functionalities like block movement, scoring, etc.
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
